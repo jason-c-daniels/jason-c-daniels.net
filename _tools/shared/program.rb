50cd735774679611d5febe692ea0c1adce7884eb
Jason Daniels <jason.c.daniels@gmail.com>
2017-12-31 13:13:06 -0600

    finished basic restructuring.

--- _tools/shared/program.rb
+++ _tools/shared/program.rb
@@ -1,39 +1,35 @@
 module Program
-   def getScriptName
-      return File.basename(caller_locations.first.path)
-   end
    class Application
-      def get_version
-         return "<version not defined>"
+      def print_usage?
+         return ARGV.length>0 && ARGV[0]=="--help";
       end
 
-      def get_usage
-         return "<usage not defined>"
+      def print_version?
+         return ARGV.length>0 && ARGV[0]=="--version";
       end
 
-      def print_version?(args)
-         return false;
-      end
-
-      def valid_args?(args)
+      def valid_args?
          return true;
       end
 
-
-      def execute(args)
-         if (valid_args?(args))
-            if print_version?(args)
-               puts "#{getScriptName} v#{get_version}"
-            else
-               main(args)
-            end
+      def execute
+         if print_version?
+            puts "#{APP_NAME} v#{APP_VERSION}"
+            exit 0
+         elsif print_usage?
+            puts APP_USAGE
+            exit 0
+         elsif valid_args?
+            main
+            exit 0
          else
-            puts get_usage
+            puts APP_USAGE
+            exit 1
          end
       end
 
-      def main(args)
-         #noop
+      def main
+         # do nothing by default
       end
    end
 end
